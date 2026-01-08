0f6d8198afad1e94fc9d4cdcbaccdfba4f183a0e
acdcnow <33558339+acdcnow@users.noreply.github.com>
2026-01-08 10:42:46 +0100

    Update Victron-Venus-Dashboard.js

--- dist/Victron-Venus-Dashboard.js
+++ dist/Victron-Venus-Dashboard.js
@@ -7,7 +7,7 @@
 */
 
 console.info(
-  "%c 🗲 %c - %cVenus OS BD%c - %c 🗲 \n%c version 0.1.17 ",
+  "%c 🗲 %c - %cVenus OS BD%c - %c 🗲 \n%c version 0.2.00 ",
   "color: white; font-weight: bold; background: black",
   "color: orange; font-weight: bold; background: blue; font-weight: bold;",
   "color: white; font-weight: bold; background: blue; text-decoration: underline; text-decoration-color: orange; text-decoration-thickness: 5px; text-underline-offset: 2px;",
@@ -188,3 +188,4 @@ window.customCards.push({
   preview: true,
   description: 'A DashBoard that looklike Venos OS gui-v2 from Victron.',
 });
+
